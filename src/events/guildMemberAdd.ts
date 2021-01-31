46c9544c2e8fea47067307c0bfda212264f96ea6
Shubham Parihar <shubhamparihar391@gmail.com>
2021-02-01 01:20:07 +0530

    feat(events): add guildMemberAdd event module

---
+++ src/events/guildMemberAdd.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+import type { GuildMember } from 'discord.js';
+import { log } from '../features/memberLogger.js';
+
+export const event = (member: GuildMember): void => {
+  log(member, 'join');
+};
