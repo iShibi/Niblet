745190d42524ea968a5d149a6dacfd6717f3633d
Shubham Parihar <shubhamparihar391@gmail.com>
2021-01-31 20:32:28 +0530

    feat(events): update message event module

--- src/events/message.ts
+++ src/events/message.ts
@@ -1,14 +1,22 @@
 import type { Message, Client, Collection } from 'discord.js';
-import type { Command } from '../index';
-const prefix = '?';
+import type { Command } from '../interfaces/Command';
 
-export const event = (message: Message, client: Client, commands: Collection<string | null, Command>): void => {
-  if (message.author.bot || !message.content.startsWith(prefix)) return;
+// this is just a hack, would remove this later
+import { createRequire } from 'module';
+const _require = createRequire(import.meta.url);
+const { PREFIX } = _require('../config.json');
 
-  const args: Array<string> = message.content.slice(prefix.length).trim().split(/ +/);
-  const command: string | null = args.shift()?.toLocaleLowerCase() ?? null;
+export const event = (message: Message, client: Client, commands: Collection<string, Command>): void => {
+  if (message.author.bot || !message.content.startsWith(PREFIX) || message.content.length <= PREFIX.length) return;
 
-  if (!commands.has(command)) return;
+  const words: Array<string> = message.content.slice(PREFIX.length).trim().split(/ +/);
+  const command: string = words[0];
+  const args: Array<string> = words.slice(1);
+
+  if (!commands.has(command)) {
+    message.channel.send('No such command exists!');
+    return;
+  }
 
   try {
     commands.get(command)?.execute(message, args);
