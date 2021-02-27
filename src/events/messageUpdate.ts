6d0970dc831a69246e59e21a0776cd786b5f344f
Shubham Parihar <shubhamparihar391@gmail.com>
2021-02-28 00:15:50 +0530

    fix: messageUpdate event emit when embed is sent

--- src/events/messageUpdate.ts
+++ src/events/messageUpdate.ts
@@ -4,5 +4,6 @@ import type { Message } from 'discord.js';
 import { logEditedMessage } from '../features/editedMessageLogger.js';
 
 export const event = (oldMessage: Message, newMessage: Message): void => {
+  if (oldMessage.content === newMessage.content) return;
   logEditedMessage(oldMessage, newMessage);
 };
