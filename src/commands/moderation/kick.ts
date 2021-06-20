a2a4fc01f6eb9bbcb1767e5aa04eaed5588c1bb9
Shubham Parihar <shubhamparihar391@gmail.com>
2021-06-20 16:16:52 +0530

    feat(kick): add button and database to kick cmd

--- src/commands/moderation/kick.ts
+++ src/commands/moderation/kick.ts
@@ -1,5 +1,9 @@
-import type { Snowflake } from 'discord.js';
-import type { InteractionCommand } from '../../interfaces/index';
+import { UserModel } from '../../schemas/User.js';
+import { createUserHistoryEmbed } from '../../utils/Utility.js';
+import { Message, MessageActionRow, MessageButton } from 'discord.js';
+import type { User } from 'discord.js';
+import type { MessageComponentInteraction } from 'discord.js';
+import type { InteractionCommand, UserSchema } from '../../interfaces/index';
 
 export const interactionCommand: InteractionCommand = {
   data: {
@@ -22,11 +26,43 @@ export const interactionCommand: InteractionCommand = {
   },
 
   async handle(interaction) {
-    await interaction.reply('Kicking...');
+    await interaction.defer();
     const author = interaction.user;
-    const member = interaction.options.get('member')?.value as Snowflake;
-    const reason = interaction.options.get('reason')?.value as string;
-    const kickedMember = await interaction.guild?.members.kick(member, `Kicked by ${author.tag} | ${reason}`);
-    interaction.editReply(`Successfully Kicked ${kickedMember}`);
+    const targetUser = interaction.options.get('member')?.user as User;
+    const reason = interaction.options.get('reason')?.value;
+    const buttonRow = new MessageActionRow().addComponents(
+      new MessageButton().setCustomID('kick').setLabel('Kick').setStyle('DANGER'),
+      new MessageButton().setCustomID('cancel_kick').setLabel('Cancel').setStyle('SECONDARY'),
+    );
+    const userDoc = await UserModel.findOne({ id: targetUser?.id }).exec();
+    const userHistoryEmbed = createUserHistoryEmbed(userDoc, targetUser);
+    await interaction.editReply({
+      content: `Do you want to kick this user?`,
+      embeds: [userHistoryEmbed],
+      components: [buttonRow],
+    });
+    const message = (await interaction.fetchReply()) as Message;
+    const filter = (i: MessageComponentInteraction) =>
+      ['kick', 'cancel_kick'].includes(i.customID) && i.user.id === author.id;
+    const response = await message.awaitMessageComponentInteraction(filter, { time: 15000 });
+    if (response.customID === 'kick') {
+      const kickedMember = await interaction.guild?.members.kick(targetUser, `Kicked by ${author.tag} | ${reason}`);
+      interaction.editReply({ content: `Successfully Kicked ${kickedMember}`, embeds: [], components: [] });
+      if (userDoc && userDoc.kicks) {
+        userDoc.kicks += 1;
+        userDoc.save();
+      } else {
+        const newUserData: UserSchema = {
+          username: targetUser?.username,
+          id: targetUser?.id,
+          tag: targetUser?.tag,
+          kicks: 1,
+        };
+        const newUserDoc = new UserModel(newUserData);
+        newUserDoc.save();
+      }
+    } else {
+      interaction.deleteReply();
+    }
   },
 };
