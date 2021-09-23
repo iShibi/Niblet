82bd8dddc6614b33109eca49258c0a69ed754ebb
Shubham Parihar <shubhamparihar391@gmail.com>
2021-09-23 13:38:08 +0530

    feat(commands): refactor user-history cmd

--- src/commands/moderation/user-history.ts
+++ src/commands/moderation/user-history.ts
@@ -1,6 +1,5 @@
 import { InteractionCommand } from '../../typings';
 import { createUserHistoryEmbed } from '../../utils/Utility';
-// import { createUserHistoryEmbed } from '../../utils/Utility';
 
 export const interactionCommand: InteractionCommand = {
   data: {
@@ -11,12 +10,15 @@ export const interactionCommand: InteractionCommand = {
 
   async handle(interaction) {
     if (!interaction.isContextMenu() || interaction.targetType !== 'USER') return;
+
     await interaction.deferReply();
-    const guild = interaction.guild;
+
+    const { guild } = interaction;
     if (!guild) {
       return interaction.editReply('This command is only valid in a guild');
     }
-    const userHistoryEmbed = await createUserHistoryEmbed(interaction.targetId, interaction.guild);
+
+    const userHistoryEmbed = await createUserHistoryEmbed(interaction.targetId, guild);
     return interaction.editReply({ embeds: [userHistoryEmbed] });
   },
 };
